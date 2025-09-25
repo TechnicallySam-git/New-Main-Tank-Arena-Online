084c9b96eec54cd9d0c62d4a587d5f1cbb799b44
TechnicallySam <YOURGAMINGNIGHTMARE_DJCREEPER@hotmail.com>
2025-09-25 13:41:20 +0200

    Decided to deploy this project on a azure linux server. I have nginx as a reverse proxy and i use eventlet to run the flask server. I have changed the code a bit to make it work with eventlet and nginx. also moved files around to make it work with nginx.
    changed tank speed to 125 to 225.

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,12 @@
+import eventlet
+eventlet.monkey_patch()
 import math
 from flask import Flask, send_from_directory, request
 from flask_socketio import SocketIO, emit
 import threading, time 
 import random
 
-app = Flask(__name__, static_folder='../frontend', static_url_path='/')
+app = Flask(__name__, static_folder='../frontend/static', static_url_path='/')
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 players = {}  # player_id = {x, y, color, health, kills, username, width, height, bodyAngle, turretAngle}
